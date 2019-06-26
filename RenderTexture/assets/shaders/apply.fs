1c2c7c7240ae03ae844f60b01b601a5eaa259804
Hasenpfote <Hasenpfote@gmail.com>
2019-06-26 21:04:00 +0900

    新規サンプルの追加

---
+++ RenderTexture/assets/shaders/apply.fs
@@ -0,0 +1,13 @@
+﻿#version 430
+
+out vec4 OutColor;
+
+uniform sampler2D texture0;
+uniform vec2 pixel_size;
+
+void main(void)
+{
+    vec2 tex_coord = gl_FragCoord.xy * pixel_size;
+    OutColor.rgb = texture(texture0, tex_coord).xyz;
+    OutColor.a = 1.0;
+}
---
+++ RenderTexture/sources/mywindow.cpp
@@ -0,0 +1,343 @@
+﻿#include <iomanip>
+#include <sstream>
+#include <memory>
+#include <GL/glew.h>
+#include <hasenpfote/assert.h>
+//#include <hasenpfote//math/utility.h>
+#include <hasenpfote//math/utils.h>
+#include <hasenpfote/math/vector3.h>
+#include <hasenpfote/math/vector4.h>
+#include <hasenpfote/math/cmatrix4.h>
+#include <hasenpfote/math/axis_angle.h>
+#include "../../Common/system.h"
+#include "../../Common/logger.h"
+#include "mywindow.h"
+
+MyWindow::MyWindow()
+{
+    LOG_D(__func__);
+}
+
+MyWindow::~MyWindow()
+{
+    LOG_D(__func__);
+}
+
+void MyWindow::Setup()
+{
+    using namespace hasenpfote::math;
+    //
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_BACK);
+
+    glDisable(GL_BLEND);
+    //glDisable(GL_LIGHTING);
+
+    glDisable(GL_MULTISAMPLE);
+
+    glClearColor(160.0f / 255.0f, 216.0f / 255.0f, 239.0f / 255.0f, 1.0);
+
+    //
+    auto window = GetWindow();
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    auto& camera = System::GetMutableInstance().GetCamera();
+
+    camera.SetViewport(0, 0, width, height);
+    //camera.Set35mmEquivalentFocalLength(50.0f);
+    camera.SetClippingPlane(1.0f, 10000.0f);
+
+    camera.SetPosition(Vector3(500.0f, 1000.0f, 1500.0f));
+    camera.SetTargetPosition(Vector3(500.0f, 0.0f, 500.0f));
+
+    camera.Update(0.0f);
+
+    // load shader.
+    {
+        auto& man = System::GetMutableInstance().GetShaderManager();
+        std::filesystem::path directory("assets/shaders");
+        man.LoadShaderPrograms(directory);
+    }
+
+    // load texture.
+    {
+        std::filesystem::path directory("assets/textures");
+        const std::filesystem::path extension(".png");
+        auto& man = System::GetMutableInstance().GetTextureManager();
+        man.LoadTextures(directory, extension);
+    }
+    // generate font.
+    {
+        std::filesystem::path fontpath = "../Common/assets/fonts/test.fnt";
+        auto font = std::make_shared<text::Font>(fontpath);
+        text = std::make_unique<SDFText>(font, std::make_shared<SDFTextRenderer>());
+        text->SetSmoothness(1.0f);
+    }
+    //
+    texture = System::GetConstInstance().GetTextureManager().GetTexture("assets/textures/testimg_1920x1080.png");
+
+    fsp_geom.Initialize();
+
+    glGenSamplers(1, &sampler);
+
+    glSamplerParameteri(sampler, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glSamplerParameteri(sampler, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_LOD_BIAS, 0.0f);
+
+    auto& man = System::GetConstInstance().GetShaderManager();
+    pipeline_fullscreen_quad.Create();
+    pipeline_fullscreen_quad.SetShaderProgram(man.GetShaderProgram("assets/shaders/fullscreen_quad.vs"));
+    pipeline_fullscreen_quad.SetShaderProgram(man.GetShaderProgram("assets/shaders/fullscreen_quad.fs"));
+
+    pipeline_apply.Create();
+    pipeline_apply.SetShaderProgram(man.GetShaderProgram("assets/shaders/apply.vs"));
+    pipeline_apply.SetShaderProgram(man.GetShaderProgram("assets/shaders/apply.fs"));
+
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("scene_rt_color", GL_RGBA8, width, height);
+        scene_rt = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("ds_rt_0_color", GL_RGBA8, width / 2, height / 2);
+        ds_rt_0 = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("ds_rt_1_color", GL_RGBA8, width / 2, height / 2);
+        ds_rt_1 = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+
+    pipeline_downsample.Create();
+    pipeline_downsample.SetShaderProgram(man.GetShaderProgram("assets/shaders/downsample.vs"));
+    pipeline_downsample.SetShaderProgram(man.GetShaderProgram("assets/shaders/downsample.fs"));
+
+    is_filter_enabled = false;
+}
+
+void MyWindow::Cleanup()
+{
+    if(glIsSampler(sampler))
+        glDeleteSamplers(1, &sampler);
+}
+
+void MyWindow::OnKey(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Window::OnKey(window, key, scancode, action, mods);
+
+    auto& camera = System::GetMutableInstance().GetCamera();
+    camera.OnKey(key, scancode, action, mods);
+
+    if(key == GLFW_KEY_M && action == GLFW_PRESS){
+        GLboolean ms;
+        glGetBooleanv(GL_MULTISAMPLE, &ms);
+        if (ms == GL_TRUE) {
+            glDisable(GL_MULTISAMPLE);
+        }
+        else {
+            glEnable(GL_MULTISAMPLE);
+        }
+    }
+    if(key == GLFW_KEY_F && action == GLFW_PRESS){
+        is_filter_enabled = !is_filter_enabled;
+    }
+}
+
+void MyWindow::OnMouseMove(GLFWwindow* window, double xpos, double ypos)
+{
+    System::GetMutableInstance().GetCamera().OnMouseMove(xpos, ypos);
+}
+
+void MyWindow::OnMouseButton(GLFWwindow* window, int button, int action, int mods)
+{
+    System::GetMutableInstance().GetCamera().OnMouseButton(button, action, mods);
+}
+
+void MyWindow::OnMouseWheel(GLFWwindow* window, double xoffset, double yoffset)
+{
+    System::GetMutableInstance().GetCamera().OnMouseWheel(xoffset, yoffset);
+}
+
+void MyWindow::OnResizeFramebuffer(GLFWwindow* window, int width, int height)
+{
+    if(HasIconified())
+        return;
+    // re-create
+    System::GetMutableInstance().GetTextureManager().DeleteTexture("foo");
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("foo", GL_RGBA8, width, height);
+        scene_rt = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("ds_rt_0_color", GL_RGBA8, width / 2, height / 2);
+        ds_rt_0 = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+    {
+        auto color = System::GetMutableInstance().GetTextureManager().CreateTexture("ds_rt_1_color", GL_RGBA8, width / 2, height / 2);
+        ds_rt_1 = std::make_unique<FrameBuffer>(color, 0, 0);
+    }
+}
+
+void MyWindow::OnResizeWindow(GLFWwindow* window, int width, int height)
+{
+    if(HasIconified())
+        return;
+    System::GetMutableInstance().GetCamera().SetViewportSize(width, height);
+}
+
+void MyWindow::OnUpdate(double dt)
+{
+    auto& camera = System::GetMutableInstance().GetCamera();
+    camera.Update(dt);
+}
+
+void MyWindow::OnRender()
+{
+    using namespace hasenpfote::math;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    auto& camera = System::GetConstInstance().GetCamera();
+    auto& vp = camera.GetViewport();
+
+    const auto width = vp.GetWidth();
+    const auto height = vp.GetHeight();
+    glViewport(0, 0, width, height);
+    //
+    scene_rt->Bind();
+    DrawFullScreenQuad();
+    scene_rt->Unbind();
+
+    auto last_rt = scene_rt.get();
+    if(is_filter_enabled){
+        last_rt = ds_rt_0.get();
+        PassDownsample(scene_rt->GetColorTexture(), last_rt);
+    }
+    PassApply(last_rt->GetColorTexture());
+
+    // 情報の表示
+    auto metrics = text->GetFont().GetFontMetrics();
+    auto line_height = static_cast<float>(metrics.GetLineHeight()); 
+    const float scale = 1.0f;
+    const float fh = line_height * scale;
+
+    static const Vector4 color(1.0f, 1.0f, 1.0f, 1.0f);
+    text->SetColor(static_cast<const GLfloat*>(color));
+
+    text->BeginRendering();
+
+    std::ostringstream oss;
+    oss << "FPS:UPS=";
+    oss << std::fixed << std::setprecision(2);
+    oss << GetFPS() << ":" << GetUPS();
+    text->DrawString(oss.str(), 0.0f, fh, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    oss << "Screen size:";
+    oss << camera.GetViewport().GetWidth() << "x" << camera.GetViewport().GetHeight();
+    text->DrawString(oss.str(), 0.0f, fh * 2.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    oss << "Aov D=" << ConvertRadiansToDegrees(camera.GetAngleOfView(CustomCamera::AngleOfView::Diagonal));
+    oss << " H=" << ConvertRadiansToDegrees(camera.GetAngleOfView(CustomCamera::AngleOfView::Horizontal));
+    oss << " V=" << ConvertRadiansToDegrees(camera.GetAngleOfView(CustomCamera::AngleOfView::Vertical));
+    text->DrawString(oss.str(), 0.0f, fh * 3.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    oss << "Focal length=" << camera.GetFocalLength() << " (35mm=" << camera.Get35mmEquivalentFocalLength() << ")";
+    text->DrawString(oss.str(), 0.0f, fh * 4.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    oss << "Zoom=x" << camera.GetZoomMagnification();
+    text->DrawString(oss.str(), 0.0f, fh * 5.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    GLboolean ms;
+    glGetBooleanv(GL_MULTISAMPLE, &ms);
+    oss << "MultiSample:" << ((ms == GL_TRUE) ? "On" : "Off") << "(Toggle MultiSample: m)";
+    text->DrawString(oss.str(), 0.0f, fh * 6.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    oss << "DownSample:" << ((is_filter_enabled == GL_TRUE) ? "On" : "Off") << "(Toggle DownSample: f)";
+    text->DrawString(oss.str(), 0.0f, fh * 7.0f, scale);
+    oss.str("");
+    oss.clear(std::stringstream::goodbit);
+
+    text->EndRendering();
+}
+
+void MyWindow::DrawFullScreenQuad()
+{
+    pipeline_fullscreen_quad.SetUniform1i("texture0", 0);
+
+    pipeline_fullscreen_quad.Bind();
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindSampler(0, sampler);
+
+    fsp_geom.Draw();
+
+    glActiveTexture(GL_TEXTURE0);
+
+    pipeline_fullscreen_quad.Unbind();
+}
+
+void MyWindow::PassDownsample(GLuint texture, FrameBuffer* fb)
+{
+    fb->Bind();
+
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    pipeline_apply.SetUniform1i("texture0", 0);
+    pipeline_apply.SetUniform2f("pixel_size", 1.0f / static_cast<float>(viewport[2]), 1.0f / static_cast<float>(viewport[3]));
+
+    pipeline_apply.Bind();
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindSampler(0, sampler);
+
+    fsp_geom.Draw();
+
+    glActiveTexture(GL_TEXTURE0);
+
+    pipeline_apply.Unbind();
+
+    fb->Unbind();
+}
+
+void MyWindow::PassApply(GLuint texture)
+{
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    pipeline_apply.SetUniform1i("texture0", 0);
+    pipeline_apply.SetUniform2f("pixel_size", 1.0f / static_cast<float>(viewport[2]), 1.0f / static_cast<float>(viewport[3]));
+
+    pipeline_apply.Bind();
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindSampler(0, sampler);
+
+    fsp_geom.Draw();
+
+    glActiveTexture(GL_TEXTURE0);
+
+    pipeline_apply.Unbind();
+}
